f2806bbbc197ec4d9788a6f16cafd47b72815d8b
RustamElkin <ryolkin90@gmail.com>
2022-08-24 17:00:31 +0600

    Думал вынести все, в итоге задание короткое)

--- src/test/java/tests/checkAvailabilityOfPagesOnGitHubTest.java
+++ src/test/java/tests/checkAvailabilityOfPagesOnGitHubTest.java
@@ -9,7 +9,7 @@ import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverConditions.url;
 
-public class checkAvailabilityOfPagesOnGithubTest {
+public class checkAvailabilityOfPagesOnGitHubTest {
 
     @BeforeAll
     static void configure() {
@@ -21,20 +21,19 @@ public class checkAvailabilityOfPagesOnGithubTest {
 
         // - Откройте страницу Selenide в Github
         open( "/selenide/selenide");
+        webdriver().shouldHave(url(baseUrl + "/selenide/selenide"));
 
         // - Перейдите в раздел Wiki проекта
         $$("nav li").findBy(text("Wiki")).click();
         webdriver().shouldHave(url(baseUrl + "/selenide/selenide/wiki"));
 
         // - Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
-        $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
-
         // - Откройте страницу SoftAssertions
+        $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
         $$("#wiki-pages-box a").findBy(exactText("SoftAssertions")).click();
         webdriver().shouldHave(url(baseUrl + "/selenide/selenide/wiki/SoftAssertions"));
 
         // - проверьте что внутри есть пример кода для JUnit5
         $(".markdown-body").shouldHave(text("Using JUnit5 extend test class")).scrollIntoView(false);
-
     }
 }
