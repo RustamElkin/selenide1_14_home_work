af1301fa28d7ec548e76e72c2b2f46beba058de9
RustamElkin <ryolkin90@gmail.com>
2022-08-24 18:01:49 +0600

    1) Изменил названия класса 3) предложили $(byText("Wiki")).click(); 4) убрал scrollIntoView(false)

--- src/test/java/tests/CheckAvailabilityOfPagesOnGitHubTest.java
+++ src/test/java/tests/CheckAvailabilityOfPagesOnGitHubTest.java
@@ -7,9 +7,8 @@ import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.WebDriverConditions.url;
 
-public class checkAvailabilityOfPagesOnGitHubTest {
+public class CheckAvailabilityOfPagesOnGitHubTest {
 
     @BeforeAll
     static void configure() {
@@ -21,19 +20,16 @@ public class checkAvailabilityOfPagesOnGitHubTest {
 
         // - Откройте страницу Selenide в Github
         open( "/selenide/selenide");
-        webdriver().shouldHave(url(baseUrl + "/selenide/selenide"));
 
         // - Перейдите в раздел Wiki проекта
         $$("nav li").findBy(text("Wiki")).click();
-        webdriver().shouldHave(url(baseUrl + "/selenide/selenide/wiki"));
 
         // - Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
         // - Откройте страницу SoftAssertions
         $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
         $$("#wiki-pages-box a").findBy(exactText("SoftAssertions")).click();
-        webdriver().shouldHave(url(baseUrl + "/selenide/selenide/wiki/SoftAssertions"));
 
         // - проверьте что внутри есть пример кода для JUnit5
-        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class")).scrollIntoView(false);
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
     }
 }
